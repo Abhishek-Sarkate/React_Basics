3e56105fca2b291af98d9c53b2919aa0db50747e
Abhishek-Sarkate <a.sarkate301198@gmail.com>
2021-02-13 00:33:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+ <div>
+          <h1>
+            Welcome to HTML
+            </h1>
+                        <h2>
+                        WELCOME TO HTML
+                      </h2>
+                                              <h3>
+                                                welcome to html
+
+                                              </h3>
+
+
+
+ </div>
+  );
+}
+
+export default App;
